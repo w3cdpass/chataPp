68254e41c741811bf190b1bd1c2835ea3b527d43
Vrecruiter-Project <vrecruitersofficial@gmail.com>
2025-04-11 15:49:00 +0530

    input

--- client/src/components/ui/MessagesFrom.jsx
+++ client/src/components/ui/MessagesFrom.jsx
@@ -1,14 +1,15 @@
-const ChataPpMessage = () => {
+const ChatLayout = () => {
   return (
-    <div class="chatCont">
-      <div class="msg1">1</div>
-      <div class="msg2">2</div>
-      <div class="msg3">3</div>
-      <div class="msg5">5</div>
-      <div class="msg6">6</div>
-      <div class="msg7">7</div>
+    <div className="flex items-center justify-between w-full !p-3 bg-gray-200 rounded-lg shadow-md">
+      <div><i className="fa-solid fa-face-smile text-red-200"></i></div>
+      <div><input type="text" placeholder="Type something" className="outline-none border-none focus:ring-0" /></div>
+      <div className="flex items-center justify-center !gap-7 w-1/3">
+      <div><i className="fa-solid fa-camera text-pink-200 text-shadow-lg/20"></i></div>
+      <div><i className="fa-solid fa-paperclip text-pink-200 text-shadow-lg/20"></i></div>
+      <div className=""><i className="fa-solid fa-microphone text-pink-200 underline text-shadow-lg/20"></i></div>
+      </div>
     </div>
   );
 };
 
-export default ChataPpMessage
+export default ChatLayout;
